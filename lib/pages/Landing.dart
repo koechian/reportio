3e52971245494f9e9fd7a5a0825a5fa7a959f8a6
Koechian <ianovic8@live.com>
2023-02-27 14:37:10 +0300

    Implemented Logout

--- lib/pages/Landing.dart
+++ lib/pages/Landing.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Landing extends StatelessWidget {
   const Landing({super.key});
 
+  void logUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(actions: [
+        IconButton(onPressed: logUserOut, icon: Icon(Icons.logout))
+      ]),
+      body: const Center(
         child: Text('Logged in'),
       ),
     );
