72498131c942c465b37206c700f42d8dd10b3e97
Koechian <ianovic8@live.com>
2023-02-28 10:57:01 +0300

    Code refactor for switcher

--- lib/pages/Auth.dart
+++ lib/pages/Auth.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:reportio/pages/Landing.dart';
-import 'package:reportio/pages/login.dart';
+import 'package:reportio/pages/switcher.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -16,7 +16,7 @@ class AuthPage extends StatelessWidget {
           if (snapshot.hasData) {
             return Landing();
           } else {
-            return const LoginPage();
+            return const Switcher();
           }
         },
       ),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:math';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../components/custom_textfield.dart';
@@ -9,7 +7,8 @@ import '../components/tile.dart';
 import '../components/auth_button.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({super.key, required this.onTap});
+  final Function()? onTap;
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -17,7 +16,6 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
-
   final passwordController = TextEditingController();
 
 // sign in user
@@ -34,6 +32,7 @@ class _LoginPageState extends State<LoginPage> {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: emailController.text, password: passwordController.text);
+      // ignore: use_build_context_synchronously
       Navigator.pop(context);
     } on FirebaseAuthException catch (e) {
 // stops the spinning loader
@@ -56,7 +55,10 @@ class _LoginPageState extends State<LoginPage> {
     showDialog(
         context: context,
         builder: (context) {
-          return AlertDialog(title: Text(message));
+          return AlertDialog(
+            title: Text(message),
+            backgroundColor: Colors.red,
+          );
         });
   }
 
@@ -66,105 +68,111 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const SizedBox(height: 50),
-            const Icon(
-              Icons.lock,
-              size: 100,
-            ),
-            const SizedBox(height: 50),
-            const Text(
-              'Welcome to Reportio',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 25),
-            MyTextField(
-              controller: emailController,
-              hint: 'Email',
-              hide: false,
-            ),
-            const SizedBox(height: 25),
-            MyTextField(
-              controller: passwordController,
-              hint: 'Password',
-              hide: true,
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const [Text('Forgot Password?')],
+          child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(height: 50),
+              const Icon(
+                Icons.lock,
+                size: 100,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            MyButton(
-              onTap: signInFunc,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey.shade400,
+              const SizedBox(height: 50),
+              const Text(
+                'Welcome to Reportio',
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: emailController,
+                hint: 'Email',
+                hide: false,
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: passwordController,
+                hint: 'Password',
+                hide: true,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [Text('Forgot Password?')],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MyButton(
+                onTap: signInFunc,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey.shade400,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Text(
-                      'Or continue with',
-                      style: TextStyle(color: Colors.grey.shade700),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey.shade700),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey.shade400,
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey.shade400,
+                      ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Tile(imagePath: 'assets/images/google.png'),
+                  SizedBox(
+                    width: 50,
                   ),
+                  Tile(imagePath: 'assets/images/apple.png'),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Tile(imagePath: 'assets/images/google.png'),
-                SizedBox(
-                  width: 50,
-                ),
-                Tile(imagePath: 'assets/images/apple.png'),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Not a whistleblower?'),
-                const SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'Signup now',
-                  style: TextStyle(
-                      color: Colors.blue[700], fontWeight: FontWeight.bold),
-                )
-              ],
-            )
-          ]),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a whistleblower?'),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  GestureDetector(
+                    onTap: widget.onTap,
+                    child: Text(
+                      'Signup now',
+                      style: TextStyle(
+                          color: Colors.blue[700], fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              )
+            ]),
+          ),
         ),
       ),
     );
