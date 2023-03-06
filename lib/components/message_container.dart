f668a44f2135d444b8ac26434ca3c4e79ef64220
koechian <ianovic8@live.com>
2023-03-06 22:18:22 +0300

    Added dynamic data to MessageContainer class

--- lib/components/message_container.dart
+++ lib/components/message_container.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class MessageContainer extends StatelessWidget {
-  const MessageContainer({super.key});
+  MessageContainer(
+      {super.key,
+      required this.messageContent,
+      required this.date,
+      required this.messageType,
+      required this.isVerified});
+
+  final String messageType;
+  final String messageContent;
+  final bool isVerified;
+  final DateTime date;
+
+  String status = 'Unverified';
+
+  Widget statusBool() {
+    if (isVerified) {
+      return Text(
+        'Verified',
+        style: GoogleFonts.rubik(
+          color: (const Color.fromRGBO(86, 163, 49, 1)),
+        ),
+      );
+    } else {
+      return Text(
+        'Unverified',
+        style: GoogleFonts.rubik(
+          color: (const Color.fromRGBO(243, 120, 104, 1)),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +73,7 @@ class MessageContainer extends StatelessWidget {
                     Row(
                       children: [
                         Text(
-                          'Traffic Update 🚦',
+                          messageType,
                           style: GoogleFonts.rubik(
                               color: Colors.white,
                               fontWeight: FontWeight.w500,
@@ -56,8 +87,7 @@ class MessageContainer extends StatelessWidget {
                     Row(
                       children: [
                         Expanded(
-                          child: Text(
-                              'Traffic congestion along Jakaya Kikwete Road ',
+                          child: Text(messageContent,
                               overflow: TextOverflow.fade,
                               softWrap: false,
                               maxLines: 1,
@@ -70,16 +100,7 @@ class MessageContainer extends StatelessWidget {
                     ),
                     Row(
                       children: [
-                        Column(
-                          children: [
-                            Text(
-                              'Verified',
-                              style: GoogleFonts.rubik(
-                                color: (const Color.fromRGBO(86, 163, 49, 1)),
-                              ),
-                            ),
-                          ],
-                        ),
+                        Column(children: [statusBool()]),
                         Expanded(
                           child: Padding(
                             padding: const EdgeInsets.only(right: 17),
@@ -87,7 +108,7 @@ class MessageContainer extends StatelessWidget {
                               crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
                                 Text(
-                                  '11:30 6th March 2023',
+                                  '${DateFormat.Hm('en_US').format(date)}, ${DateFormat.MMMMEEEEd('en_US').format(date)}',
                                   style: GoogleFonts.rubik(
                                     color: (const Color.fromARGB(
                                         255, 175, 175, 175)),
