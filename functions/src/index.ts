a3f85f82900975d980f17b987b15dfd1d1b9055d
koechian <ianovic8@live.com>
2023-03-13 11:44:17 +0300

    Written function to add message to pending col

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,17 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+
+admin.initializeApp();
+const db = admin.firestore();
 
 export const pushToPending = functions.firestore
   .document("messages/{isVerified}")
   .onUpdate((change, context) => {
     const message = change.after.data();
 
-    functions.logger.log(message);
+    db.doc("pending/").set(message);
   });
